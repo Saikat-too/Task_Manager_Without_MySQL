6b356f64049d9672f3f03991e6807e12555e4e21
Saikat-too <singhasaikat37@gmail.com>
2024-04-28 19:32:52 +0600

    set taskroutes

--- src/app.js
+++ src/app.js
@@ -4,4 +4,8 @@ const taskroutes = require('./routes/taskroutes');
 
 // Setting up express ap and configure it to parse incoming json request 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+
+// Setting up my express.js app to use the taskroutes to for request from 'tasks' path
+app.use('/tasks',taskroutes);
+
