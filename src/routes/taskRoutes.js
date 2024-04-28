2d2fde6d5536b94b4cd448a139dc426a4e930459
Saikat-too <singhasaikat37@gmail.com>
2024-04-28 19:49:03 +0600

    setup routes for CRUD

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -5,3 +5,12 @@ const taskController = require ('../controllers/taskController');
 
 //Creating ne routing object
 const router = express.Router();
+
+// Set up routes for CRUD
+
+router.post('/', taskController.createTask);
+router.get('/', taskController.getAllTasks);
+router.put('/:id', taskController.updateTask);
+router.delete('/:id', taskController.deleteTask);
+
+module.exports = router
