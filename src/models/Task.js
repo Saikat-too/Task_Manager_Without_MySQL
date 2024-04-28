0afe03215462af13f7f6d8ff7b6bd33ea30b2de4
Saikat-too <singhasaikat37@gmail.com>
2024-04-28 19:53:10 +0600

    Task class creating and setting up to manage the tasks

---
+++ src/models/Task.js
@@ -0,0 +1,13 @@
+// Creating a Task class 
+
+class Task{
+
+    constructor(id,title,description,status){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+}
+
+module.exports = Task; 
