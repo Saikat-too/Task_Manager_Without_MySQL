243258d0daa9bdd43dac92a2f699aa63a6f3260f
Saikat-too <singhasaikat37@gmail.com>
2024-04-28 20:08:28 +0600

    Creating a task in script file

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,2 +1,15 @@
 // Importing task.js in this script file
 const Task = require('../models/Task');
+
+//In memory Storage
+
+const tasks = [];
+let nextId = 1;
+
+const createTask = (req , res) => {}
+    const {title,descripton,status } = req.body;
+    const newTask = new Task(nextId++ , title , descripton , status );
+    tasks.push(newTask);
+    res.status(201).json(newTask);
+
+;
